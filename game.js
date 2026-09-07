41c3b1f10fbc3b47fc9deb8c2705ff6076fcdad5
Casbari <116699790+addsarah@users.noreply.github.com>
2026-09-07 19:44:21 +0700

    task 1 07092026

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "cyan";
-ctx.fillRect(100, 100, 40, 40);
+// 1. Menggambar Player (Warna, ukuran, dan posisi sudah diubah)
+ctx.fillStyle = "lime"; // Mengubah warna player menjadi hijau
+ctx.fillRect(50, 200, 60, 60); // x: 50, y: 200, lebar: 60, tinggi: 60
+
+// 2. Menggambar Objek Target
+ctx.fillStyle = "red"; // Warna target berbeda (merah)
+ctx.fillRect(600, 300, 30, 30); // Posisi target jauh dari player
